258469d2f6b0b9849e64cb3bf54a1bc24943ba2d
FelixLuciano <felix.lucianodias@gmail.com>
2024-05-25 18:29:23 -0300

    Refactor preprocessing module for improved code organization and readability

--- scripts/preprocess.py
+++ scripts/preprocess.py
@@ -1,62 +1,50 @@
 import os
+from pathlib import Path
 
 import pandas as pd
 
 
 class Script:
-
     ticker: str
     data_dir: str
 
-    def __init__(self, ticker):
+    def __init__(self, ticker: str, basedir: Path = Path.cwd() / "data"):
         """Download data, preprocess and save it to a file."""
         self.ticker = ticker
-        self.data_dir = f"data/{ticker}"
-
-    def _DownloadData(self):
-        """Download data from Binance API."""
-        pass
-
-    def GetData(self):
-        list_of_dataframes = []
-
-        for file in os.listdir(self.data_dir):
-            if file.endswith(".csv"):
-
-                dataframe = pd.read_csv(
-                    f"{self.data_dir}/{file}",
-                    usecols=[
-                        "best_ask_price"
-                        "best_ask_qty"
-                        "best_bid_price"
-                        "best_bid_qty"
-                        "event_time"
-                    ],
-                )
-
-                list_of_dataframes.append(dataframe)
-
-        return list_of_dataframes
-
-    def _Preprocess(self):
-        self._DownloadData()
-
-        list_of_dataframes = self.GetData()
-
-        list_of_preprocessed_dataframes = [
-            dataframe.assign(
-                mid_price=(dataframe["best_ask_price"] + dataframe["best_bid_price"])
-                / 2
-            )
-            for dataframe in list_of_dataframes
-        ]
-
-        return list_of_preprocessed_dataframes
+        self.data_dir = basedir / ticker
     
-    def SaveData(self):
-        list_of_preprocessed_dataframes = self._Preprocess()
+    def preprocess(self):
+        preprocessed_dataframes = self._process_dataframes()
 
-        for i, dataframe in enumerate(list_of_preprocessed_dataframes):
-            dataframe.to_parquet(f"{self.data_dir}/{self.ticker}-day_{i}.parquet", index=False)
+        for index, dataframe in enumerate(preprocessed_dataframes):
+            filename = self.data_dir / f"{self.ticker}-day_{index}.parquet"
 
-        return True
+            dataframe.to_parquet(filename, index=False)
+
+    def _process_dataframes(self):
+        for dataframe in self._load_dataframes():
+            mid_price = (dataframe["best_ask_price"] + dataframe["best_bid_price"]) / 2
+
+            yield dataframe.assign(mid_price=mid_price)
+
+    def _load_dataframes(self):
+        for file in os.listdir(self.data_dir):
+            if not file.endswith(".csv"):
+                continue
+
+            yield pd.read_csv(
+                f"{self.data_dir}/{file}",
+                usecols=[
+                    "best_ask_price",
+                    "best_ask_qty",
+                    "best_bid_price",
+                    "best_bid_qty",
+                    "event_time",
+                ],
+            )
+
+if __name__ == "__main__":
+    tickers = ["ADA", "AXS", "BTC", "DOGE", "NEAR"]
+
+    for ticker in tickers:
+        Script(ticker).preprocess()

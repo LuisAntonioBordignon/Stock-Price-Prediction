29de8744d50572dfc3302cb16c2faf56dda4d94e
Luís Antônio <luisabo@al.insper.edu.br>
2024-05-30 19:06:52 -0300

    Refactor MLP class for improved code organization and readability

--- src/lib/metrics.py
+++ src/lib/metrics.py
@@ -1,13 +1,42 @@
 import pandas as pd
 import numpy as np
 
+"""
+https://neptune.ai/blog/performance-metrics-in-machine-learning-complete-guide
+"""
+
 class Metrics:
 
     def __init__(self):
         pass
 
+    def MSE(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
+        """Mean Squared Error"""
+        MSE = np.mean((y_true - y_pred) ** 2)
+
+        return MSE
+    
+    def MAE(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
+        """Mean Absolute Error"""
+        MAE = np.mean(np.abs(y_true - y_pred))
+
+        return MAE
+
     def RMSE(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
+        """Root Mean Squared Error"""
         RMSE = np.sqrt(np.mean((y_true - y_pred) ** 2))
 
         return RMSE
+    
+    def MAPE(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
+        """Mean Absolute Percentage Error"""
+        MAPE = np.mean(np.abs((y_true - y_pred) / y_true)) * 100
+
+        return MAPE
+    
+    def R2(self, y_true: pd.DataFrame, y_pred: pd.DataFrame):
+        """R-squared"""
+        R2 = 1 - np.sum((y_true - y_pred) ** 2) / np.sum((y_true - np.mean(y_true)) ** 2)
+
+        return R2
 
--- src/models/MLP.py
+++ src/models/MLP.py
@@ -10,21 +10,21 @@ from tensorflow.keras.callbacks import EarlyStopping # type: ignore
 
 
 class MLP:
-    def __init__(self, epochs: int = 10, batch_size: int = 32):
+    def __init__(self, epochs: int = 10, batch_size: int = 256):
         self.epochs = epochs
         self.batch_size = batch_size
         self.model = Sequential()
 
         self.model.add(Input(shape=(4,)))
-        self.model.add(Dense(8, activation="relu"))
-        self.model.add(Dense(8, activation="relu"))
-        self.model.add(Dense(3, activation="softmax"))
-        self.model.compile(
-            optimizer="adam", loss="categorical_crossentropy", metrics=["accuracy"]
-        )
+        self.model.add(Dense(512, activation="elu"))
+        self.model.add(Dense(512, activation="elu"))
+        self.model.add(Dense(32, activation="elu"))
+        self.model.add(Dense(10))
+
+        self.model.compile(loss="mean_squared_error", optimizer="adam", metrics=["mean_squared_error"])
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.DataFrame):
-            early_stopping = EarlyStopping(monitor='val_loss', patience=2)
+            early_stopping = EarlyStopping(monitor="val_loss", patience=2)
 
             return self.model.fit(
                 X_train,

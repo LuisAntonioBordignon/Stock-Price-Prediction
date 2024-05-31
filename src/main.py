533baef6358f070bc30ce2a033d7cd8d05306a86
Luís Antônio <luisabo@al.insper.edu.br>
2024-05-31 14:29:20 -0300

    Refactor main.py for improved code organization and readability

--- src/main.py
+++ src/main.py
@@ -7,38 +7,40 @@ from src.models import *
 
 TRAINING_DAYS = 5
 tickers = list(Path("data/tickers/").glob("**/"))[1:]
-histories = {}
 
 for ticker in tickers:
     model = MLP()
     name = ticker.name
-    histories[name] = []
+    histories = []
 
-    datasets = ticker.glob(f"*.parquet") 
+    datasets = ticker.glob(f"*.parquet")
 
     print(f"Estamos no ticker {name}!")
 
-    for day, ticker in enumerate(tickers, 1):
+    for day in range(1, TRAINING_DAYS + 1):
 
-        print(f"Dia {day}...")
+        print(f"Dia {day} no ticker {name}!")
 
-        if day > TRAINING_DAYS:
-            print(f"BREAK!!!!!")
-            break
-
-        X_train = pd.read_parquet(ticker, columns=["best_bid_price", "best_ask_price", "best_bid_qty", "best_ask_qty"])
+        X_train = pd.read_parquet(
+            ticker,
+            columns=[
+                "best_bid_price",
+                "best_ask_price",
+                "best_bid_qty",
+                "best_ask_qty",
+            ],
+        )
         y_train = pd.read_parquet(ticker, columns=["mid_price"])
 
+        X_train = X_train.iloc[:100, :]
+        y_train = y_train.iloc[:100, :]
+
         history = model.fit(X_train, y_train)
-        history = pd.DataFrame(history.history)
-        histories[name].append(history)
+        histories.append(history)
 
-    for ticker, history_list in histories.items():
+    histories = pd.concat([pd.DataFrame(history.history) for history in histories])
 
-        print(len(history_list))
-        print(len(history_list[0]))
-
-        history_df = pd.concat(history_list)
-        history_df.to_parquet(f"data/histories/{type(model).__name__}-{ticker}.parquet")
-
+    histories.to_parquet(
+        f"data/histories/{type(model).__name__}-{name}.parquet"
+    )
     model.model.save(f"data/models/{type(model).__name__}-{name}.keras")

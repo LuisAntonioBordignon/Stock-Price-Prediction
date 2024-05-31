a6670cfa0791aebb8e2a051d30302584fd7058e2
Luís Antônio <luisabo@al.insper.edu.br>
2024-05-31 13:57:07 -0300

    Refactor main.py for improved code organization and readability

--- src/main.py
+++ src/main.py
@@ -10,26 +10,35 @@ tickers = list(Path("data/tickers/").glob("**/"))[1:]
 histories = {}
 
 for ticker in tickers:
+    model = MLP()
     name = ticker.name
     histories[name] = []
-    model = MLP()
+
     datasets = ticker.glob(f"*.parquet") 
 
+    print(f"Estamos no ticker {name}!")
+
     for day, ticker in enumerate(tickers, 1):
+
+        print(f"Dia {day}...")
+
         if day > TRAINING_DAYS:
             print(f"BREAK!!!!!")
             break
 
-        X_train = pd.read_parquet(ticker)
-        y_train = X_train["mid_price"]
-
-        X_train.drop(columns=["mid_price"], inplace=True)
+        X_train = pd.read_parquet(ticker, columns=["best_bid_price", "best_ask_price", "best_bid_qty", "best_ask_qty"])
+        y_train = pd.read_parquet(ticker, columns=["mid_price"])
 
         history = model.fit(X_train, y_train)
-
+        history = pd.DataFrame(history.history)
         histories[name].append(history)
 
-    print(f"Saving model {name}...")
-    model.model.save(f"data/models/{name}.keras")
+    for ticker, history_list in histories.items():
 
-print(f"histories: {histories}")
+        print(len(history_list))
+        print(len(history_list[0]))
+
+        history_df = pd.concat(history_list)
+        history_df.to_parquet(f"data/histories/{type(model).__name__}-{ticker}.parquet")
+
+    model.model.save(f"data/models/{type(model).__name__}-{name}.keras")
--- src/models/LSTM.py
+++ src/models/LSTM.py
@@ -1,17 +1,34 @@
 import os
-
 os.environ["TF_CPP_MIN_LOG_LEVEL"] = "2"
 
+from tensorflow.keras.layers import LSTM as LSTM_layer, Dense, Input # type: ignore
+from tensorflow.keras.models import Sequential # type: ignore
+from tensorflow.keras.callbacks import EarlyStopping # type: ignore
 import pandas as pd
-from tensorflow.keras.layers import Dense, Input
-from tensorflow.keras.models import Sequential
-
 
 class LSTM:
-    def __init__(self, epochs: int = 10, batch_size: int = 32):
+    def __init__(self, epochs: int = 1, batch_size: int = 256):
         self.epochs = epochs
         self.batch_size = batch_size
-        self.model = None
+        self.model = Sequential()
+
+        self.model.add(Input(shape=(1, 4)))
+        self.model.add(LSTM_layer(units=128, activation="tanh"))
+        self.model.add(Dense(units=10))
+        self.model.compile(optimizer="adam", loss="mean_squared_error", metrics=["mean_squared_error"])
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series):
-        return None
+        # Reshape X_train to be 3D [samples, timesteps, features]
+        X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
+
+        early_stopping = EarlyStopping(monitor="val_loss", patience=2)
+
+        return self.model.fit(
+            X_train,
+            y_train,
+            epochs=self.epochs,
+            batch_size=self.batch_size,
+            verbose=1,
+            callbacks=[early_stopping],
+            validation_split=0.2, 
+        )

8a4f561997c8c011d4819445ee45d5798ccfc011
Luís Antônio <luisabo@al.insper.edu.br>
2024-06-03 19:02:18 -0300

    refactor: Update LSTM model initialization and prediction method

--- src/models/LSTM.py
+++ src/models/LSTM.py
@@ -9,30 +9,40 @@ import pandas as pd
 
 
 class LSTM:
-    def __init__(self, epochs: int = 10, batch_size: int = 8):
+    def __init__(self, n_features: int, epochs: int, batch_size: int):
         self.epochs = epochs
         self.batch_size = batch_size
         self.model = Sequential()
 
-        self.model.add(Input(shape=(1, 17)))
-        self.model.add(LSTM_layer(units=4, activation="tanh", kernel_regularizer=L1L2(0.03, 0.03)))
-        self.model.add(Dropout(0.5))
+        self.model.add(Input(shape=(1, n_features)))
+
+        self.model.add(LSTM_layer(512, activation="elu", return_sequences=True))
+        self.model.add(LSTM_layer(512, activation="elu", return_sequences=True))
+        self.model.add(LSTM_layer(4, activation="elu"))
+
         self.model.compile(optimizer="adam", loss="mean_squared_error", metrics=["mean_squared_error"])
 
     def fit(self, X_train: pd.DataFrame, y_train: pd.Series):
-        # Reshape X_train to be 3D [samples, timesteps, features]
-        X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
 
+        X_train = X_train.values.reshape((X_train.shape[0], 1, X_train.shape[1]))
+        
         return self.model.fit(
             X_train,
             y_train,
             epochs=self.epochs,
             batch_size=self.batch_size,
-            verbose=1,
+            verbose=0,
             validation_split=0.2, 
         )
     
-        def predict(self, X):
-            # Reshape X to be 3D [samples, timesteps, features]
-            X = X.values.reshape((X.shape[0], 1, X.shape[1]))
-            return self.model.predict(X)
+
+    def predict(self, X_test: pd.DataFrame):
+
+        X_test_index = X_test.index
+        X_test = X_test.values.reshape((X_test.shape[0], 1, X_test.shape[1]))
+
+        return pd.DataFrame.from_records(
+            data=self.model.predict(X_test),
+            index=X_test_index,
+            columns=("Open", "High", "Low", "Close")
+        )
